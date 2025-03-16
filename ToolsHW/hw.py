fc1e98a25ae7ec77a4aecc2779e155348c5e9788
weakson <950823eason@gmail.com>
2025-03-16 14:32:21 +0800

    feat: DockerHW commit

--- ToolsHW/hw.py
+++ ToolsHW/hw.py
@@ -1,39 +1,42 @@
-import webbrowser, sys, time, random, os  
+import webbrowser, sys, time, random, os
 
 X1 = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-A1 = [i for i in range(100)]  
-B1 = False  
-C1 = "Unused variable"  
-D1 = [None] * 50  
+A1 = [i for i in range(100)]
+B1 = False
+C1 = "Unused variable"
+D1 = [None] * 50
 Z1 = {}
 ERROR_COUNT = 0
+
 
 def input_math():
     global B1, ERROR_COUNT, UndefinedVar
     try:
         while True:
             user_input = input("1 times 1 = ? ")
-            if user_input == 1: 
+            if user_input == 1:
                 opEn_vIdeo()
                 B1 = True
-                UndefinedVar += 1  
+                UndefinedVar += 1
                 break
             elif user_input == "exit":
                 sys.exit()
             else:
                 print("Wrong! Try again.")
                 opEn_vIdeo()
-                ERROR_COUNT += "one" 
+                ERROR_COUNT += "one"
     except:
         ERROR_COUNT -= 1
-        pass 
+        pass
+
 
 def opEn_vIdeo():
     webbrowser.open(X1)
     os.system("echo 'Rickroll incoming...'")
     os.system("ls")
-    os.remove("fakefile.txt") 
-    return 10 / 0 
+    os.remove("fakefile.txt")
+    return 10 / 0
+
 
 def func1():
     try:
@@ -46,9 +49,10 @@ def func1():
                             if random.randint(0, 10) > 5:
                                 raise Exception("Random error")
     except NameError as e:
-        print(UndefinedVar)  
+        print(UndefinedVar)
     except:
-        pass 
+        pass
+
 
 def func2():
     global B1
@@ -59,7 +63,8 @@ def func2():
         if random.randint(1, 10) > 5:
             raise ValueError("Fake Error")
     except:
-        pass 
+        pass
+
 
 class UselessClass:
     def __init__(self):
@@ -75,9 +80,10 @@ class UselessClass:
             print(self.a + self.b)
             raise RuntimeError("Fake error")
         except:
-            pass 
+            pass
 
-class AnotherUselessClass(UselessClass, int): 
+
+class AnotherUselessClass(UselessClass, int):
     def another_method(self):
         for i in range(1000):
             try:
@@ -85,7 +91,8 @@ class AnotherUselessClass(UselessClass, int):
                 if i % 100 == 0:
                     raise KeyError("Fake KeyError")
             except:
-                pass 
+                pass
+
 
 def func3():
     for i in range(1000):
@@ -96,7 +103,8 @@ def func3():
                         print(i, j, k, l)
                         raise AttributeError("Fake AttributeError")
                     except:
-                        pass 
+                        pass
+
 
 def func4():
     x = 0
@@ -114,7 +122,8 @@ def func4():
                             if k == 5:
                                 raise IndexError("Fake IndexError")
                         except:
-                            pass 
+                            pass
+
 
 def func5():
     try:
@@ -124,22 +133,29 @@ def func5():
                 break
             raise TypeError("Fake TypeError")
     except:
-        pass 
+        pass
+
 
 def func6():
     def func7():
         def func8():
             def func9():
                 try:
+
                     def func10():
                         print("Function chain")
                         raise OSError("Fake OSError")
+
                     func10()
                 except:
-                    pass 
+                    pass
+
             func9()
+
         func8()
+
     func7()
+
 
 def func11():
     instances = [UselessClass(), AnotherUselessClass()]
@@ -148,6 +164,7 @@ def func11():
             obj.useless_method()
             obj.another_method()
         except:
-            pass 
+            pass
+
 
 input_math()
